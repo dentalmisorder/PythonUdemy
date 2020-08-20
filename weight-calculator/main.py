69f3a99d091dde613225a76efec81d16caa32607
doulikedarkness <denys.bondarenko@nure.ua>
2020-08-20 16:02:32 +0300

    simple weight converter

---
+++ weight-calculator/main.py
@@ -0,0 +1,33 @@
+#unfortunately python doesnt have multi-line comments :c
+#weight calculator for dummies
+
+tutorial = """
+================｢HOW TO USE?｣================
+
+『simply type in ur weight first, then system to convert』
+『K - for kilograms』
+『L - for pounds』
+
+github: ｢https://github.com/doulikedarkness｣
+
+================｢HOW TO USE?｣================
+"""
+
+print(tutorial)
+
+weight_from_user = float(input("Tell us ur weight (no worries no data will be saved): "))
+
+calculate_system = input("What do u want us to convert it into? (L)bs or (K)gs: ")
+
+if calculate_system.upper() == "L":
+    converted_weight = weight_from_user * 2.205
+    print(f"{weight_from_user} kilograms its around~ {converted_weight} pounds")
+elif calculate_system.upper() == "K":
+    converted_weight = weight_from_user / 2.205
+    print(f"{weight_from_user} pounds its around~ {converted_weight} kilograms")
+else:
+    print(f"\nDude what did u just typed in? \nHow do we turn {weight_from_user} into ur '{calculate_system}'?")
+    print("[RU] Все что от тебя требуется это написать вес а потом букву, по какой системе ты хочешь конвертировать. \nK - в килограмы, L - в фунты")
+    print(tutorial)
+    
+#endregion (yea ikr its not C#)
