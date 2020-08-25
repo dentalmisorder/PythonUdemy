612b713b8def87e988be5b5acdc6e36c549734a2
doulikedarkness <denys.bondarenko@nure.ua>
2020-08-25 19:57:13 +0300

    tuples and unpacking

---
+++ basics (not actual projects)/tuple.py
@@ -0,0 +1,8 @@
+divisions = ("iron", "bronze", "silver", "gold", "platinum", "diamond", "master", "challenger")
+
+admins = ("Spylestia", "Lesteeh", "furrypussy femboy")
+
+admin0, admin1, admin2 = admins #unpacking
+
+print(admin1)
+print(admin2)
