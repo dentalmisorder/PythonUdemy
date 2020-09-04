fafcc84fd904987aa1003b69ef1ff6d193c6691c
doulikedarkness <denys.bondarenko@nure.ua>
2020-09-04 19:47:23 +0300

    slicing and dictionaries

---
+++ basics (not actual projects)/dictionaries.py
@@ -0,0 +1,23 @@
+#dictionary
+
+numbers = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "0": "Tsero"
+}
+
+input_user = input("Phone: ")
+
+nums = input_user.split()
+
+for num in nums:
+    print(num)
+
+#print(numbers[input_user.rsplit()])
---
+++ basics (not actual projects)/slicing.py
@@ -0,0 +1,7 @@
+my_name = "Saphira"
+name_length = len(my_name)
+
+print(my_name[0:-2:2]) #slice FROM letter TO letter, with STEP
+print(my_name[0:6]) #result is Saphir cause count starts from 0 not from 1
+
+print(name_length) #result is 7
