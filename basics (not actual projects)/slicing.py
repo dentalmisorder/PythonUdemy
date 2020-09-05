159d066e5ff5996f34753ba212ee8714c3d795c1
doulikedarkness <denys.bondarenko@nure.ua>
2020-09-05 14:20:18 +0300

    slicing stuff

--- basics (not actual projects)/slicing.py
+++ basics (not actual projects)/slicing.py
@@ -3,5 +3,6 @@ name_length = len(my_name)
 
 print(my_name[0:-2:2]) #slice FROM letter TO letter, with STEP
 print(my_name[0:6]) #result is Saphir cause count starts from 0 not from 1
+print(my_name[2:]) #result FROM letter with index 2 and ALL after index 2
 
 print(name_length) #result is 7
