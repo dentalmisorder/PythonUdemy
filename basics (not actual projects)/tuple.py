52c939df69ae05bde795b40a2c0e71e4a2c672d5
doulikedarkness <denys.bondarenko@nure.ua>
2020-09-09 21:24:32 +0300

    more tuple practice

--- basics (not actual projects)/tuple.py
+++ basics (not actual projects)/tuple.py
@@ -1,6 +1,15 @@
 divisions = ("iron", "bronze", "silver", "gold", "platinum", "diamond", "master", "challenger")
 
 admins = ("Spylestia", "Lesteeh", "furrypussy femboy")
+genders = ("man", "woman")
+genders_that_can_be_changed = ["man", "woman"]
+
+man, woman = genders
+print(f'genders_that_can_be_changed: {genders_that_can_be_changed},')
+genders_that_can_be_changed.append("trap")
+print(f'now we add one more: {genders_that_can_be_changed}')
+print(f'genders: {genders} but we cant add anything cause its tuple but we can unpack something: {woman}, {man}')
+
 
 admin0, admin1, admin2 = admins #unpacking
 
