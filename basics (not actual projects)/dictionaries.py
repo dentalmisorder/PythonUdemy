47ab78821e5f6c682a4ac93ffde539dfb2ad3fbc
doulikedarkness <denys.bondarenko@nure.ua>
2020-09-09 09:04:03 +0300

    more dictionaries stuff and practice

--- basics (not actual projects)/dictionaries.py
+++ basics (not actual projects)/dictionaries.py
@@ -13,9 +13,30 @@ numbers = {
     "0": "Tsero"
 }
 
-input_user = input("Phone: ")
+input_user = input("Insert numbers with space: ")
 
 nums = input_user.split()
 
 for num in nums:
     print(numbers[num])
+
+#=======================================
+
+prices = {
+    "apple":2.99,
+    "orange":5.99,
+    "banana":3.99,
+    "ores":{
+        "iron":9.99,
+        "coal":6.99,
+        "gold":18.99,
+        "platinum":16.50
+    }
+}
+
+print(f'\nprice of an apple is {prices["apple"]}') #simple use with key-value pair
+print(f'\nprice of an ore -> coal is: {prices["ores"]["coal"]}') #coal cost in dictionary inside of another dictionary
+
+prices["ores"]["diamond"] = 99.99 #adding one more ore
+
+print(f'\nprice of all ores: {prices["ores"]}') #list of all ores
