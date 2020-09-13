8f17b70a0d162f039f167d0d209f05f14dda0b1f
doulikedarkness <denys.bondarenko@nure.ua>
2020-09-13 22:37:59 +0300

    logical and boolean basics

---
+++ basics (not actual projects)/logicalNbool.py
@@ -0,0 +1,12 @@
+#boolean
+
+print(2 > 1) #true 2 is greater then 1
+print(2 <= 1) #false 2 is not lower or equal then 1
+print(2 != 1) #true 2 is not equal 1
+print(2 == 2) #true 2 is equal 2
+
+#logical
+
+print(2 == 2 and 2 > 1) #true, 2 is equal to 2 and 2 greater then 1
+print(2 == 2 or 2 < 1) #true, 2 is equal to 2 (cause OR operator needs only 1 true)
+print(not 2 == 2) #false cause NOT operator gets the oposite of what expected (expected true)
