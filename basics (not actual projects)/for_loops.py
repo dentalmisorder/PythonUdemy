117dcb2cfb1b783c4d456df63bb9a64eb0df0028
doulikedarkness <denys.bondarenko@nure.ua>
2020-09-14 19:47:51 +0300

    more for loops practice

--- basics (not actual projects)/for_loops.py
+++ basics (not actual projects)/for_loops.py
@@ -6,3 +6,17 @@ for price in prices:
     print(f"Adding item that costs {price}$")
 
 print(f"Total: {total_price}")
+
+#more practice
+
+for letter in 'hewwo':
+    print(letter) #u can print every letter in string cause its also a list of letters
+
+my_published_games = {
+    'Anime tiddie goths': 2.99,
+    'Witch nekos': 5.99,
+    'Farm and grow': 9.99
+}
+
+for game_name, game_price in my_published_games.items():
+    print(f'Game title: {game_name} | Price: {game_price}$')
