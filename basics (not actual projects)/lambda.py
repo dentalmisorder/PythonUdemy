7001ead8861d3bd07685914c1f14f0da8a4ebc18
doulikedarkness <denys.bondarenko@nure.ua>
2020-09-25 20:48:25 +0300

    filter and map funcs

--- basics (not actual projects)/lambda.py
+++ basics (not actual projects)/lambda.py
@@ -11,3 +11,9 @@ num_list = [1,2,3,4,5,6]
 
 map_list = list(map(lambda num: num**2, num_list))
 print(map_list)
+
+#more expressions
+num_list = [1,2,3,4,5,6]
+
+filter_list = list(filter(lambda num: num%2==0, num_list))
+print(filter_list) #filter gets exactly the value if it is true, instead of just getting the True
