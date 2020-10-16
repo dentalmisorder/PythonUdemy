246b712b07f5aaf10510266132a8f0b9949fcb9a
doulikedarkness <denys.bondarenko@nure.ua>
2020-10-16 19:12:21 +0300

    classes things

---
+++ basics (not actual projects)/oop/class.py
@@ -0,0 +1,6 @@
+class Client():
+    def __init__(self, bank_count, account_name):
+        self.bank_money_count = bank_count
+        self.bank_account_name = account_name
+
+client1337 = Client(account_name="Denys", bank_count=100)
