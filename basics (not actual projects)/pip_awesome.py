5853dd17f56652545fbca20303801d4964bb6624
Denys Bondarenko <denys.bondarenko@nure.ua>
2020-11-07 19:35:14 +0200

    colorama n pip practice googlin xd

---
+++ basics (not actual projects)/pip_awesome.py
@@ -0,0 +1,8 @@
+from colorama import init
+init()
+from colorama import Fore
+
+print(Fore.MAGENTA + "All i want from life is small tiddie goth anime gf")
+
+#if not Windows u should call deinit() before program exits
+#deinit()
