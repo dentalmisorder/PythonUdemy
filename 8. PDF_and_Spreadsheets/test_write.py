9c2c2ee81617866db5cd60563456781317453446
Denys Bondarenko <denys.bondarenko@nure.ua>
2020-12-23 20:21:59 +0200

    more writing practice

--- 8. PDF_and_Spreadsheets/test_write.py
+++ 8. PDF_and_Spreadsheets/test_write.py
@@ -7,3 +7,6 @@ writer = csv.writer(file,delimiter=';')
 writer.writerow(['1', 'Freyja', 'Grandmaster', 'https://www.artstation.com/doulikedarkness'])
 writer.writerow(['2', 'doulikedarkness', 'Master', 'https://vk.com/doulikedarkness'])
 writer.writerow(['3', 'Spylestia', 'Diamond', 'https://discord.gg/NPKTF3Nbad'])
+writer.writerows([['4', 'Holopsicon', 'Challenger', 'https://vk.com/videos-159557549'], ['5', 'Freyja_Zero', 'Grandmaster', 'https://vk.com/album-159557549_250729025']])
+
+file.close()
