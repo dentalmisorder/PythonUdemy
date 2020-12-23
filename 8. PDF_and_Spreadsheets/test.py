cbb6f096d618ed0124bcd84c448c42dfe965d572
Denys Bondarenko <denys.bondarenko@nure.ua>
2020-12-23 19:54:18 +0200

    csv practice chosing only females from people list

--- 8. PDF_and_Spreadsheets/test.py
+++ 8. PDF_and_Spreadsheets/test.py
@@ -5,14 +5,17 @@ file_to_read = open('8. PDF_and_Spreadsheets\\example.csv',encoding='utf-8')
 #file_to_read.seek(0)
 
 csv_file = list(csv.reader(file_to_read))
+how_many_results = 100
 
 print(f'''
+First {how_many_results} results:
 =================================================================================
 {csv_file[0]}
 =================================================================================
 ''')
 
-for line in csv_file[1:5]:
-    print(line)
+for line in csv_file[1:how_many_results]:
+    if 'female' in line[4].lower():
+        print(f'[ID: {line[0]}] | Name: {line[1]} {line[2]} | Gender: {line[4]} | Address: {line[6]}')
 
 file_to_read.close()
